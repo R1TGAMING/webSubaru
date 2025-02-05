93bb9ada746773fd4150b9d55cfc0bc4fddd7109
ipii <Ipii@rafisofyantriyanto02@gmail.com>
2025-02-05 23:05:40 +0700

    section

--- script.js
+++ script.js
@@ -7,3 +7,21 @@ function clickNav(e) {
     ? ((e.name = "close-outline"), listnav.classList.add("opacity-100"))
     : ((e.name = "menu-outline"), listnav.classList.remove("opacity-100"));
 }
+
+ let index = 0;
+displayImages();
+function displayImages() {
+  let i;
+  const images = document.getElementsByClassName("mySlides");
+  for (i = 0; i < images.length; i++) {
+    images[i].style.display = "none";
+  }
+  index++;
+  if (index > images.length) {
+    index = 1;
+  }
+  images[index-1].style.display = "block";
+  setTimeout(displayImages, 2000); 
+}
+
+
